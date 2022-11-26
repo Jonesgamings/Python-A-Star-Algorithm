3e057e82e364dbc7fc30eba661ddf13482891ffb
Jonesgamings <harry.tennyson.jones@gmail.com>
2022-11-26 15:55:15 +0000

    Main

---
+++ aStar.py
@@ -0,0 +1,77 @@
+from grid import Grid, Node
+import pygame
+
+def Astar(grid: Grid, start: Node, end: Node):
+    openNodes = []
+    closedNodes = [] 
+
+    openNodes.append(start)
+
+    while len(openNodes) > 0:
+        currentNode = openNodes[0]
+        currentIndex = 0
+
+        for index, node in enumerate(openNodes):
+            if node.f < currentNode.f:
+                currentNode = node
+                currentIndex = index
+
+        openNodes.pop(currentIndex)
+        closedNodes.append(currentNode)
+
+        if currentNode.pos == end.pos:
+            path = []
+            current = currentNode
+            while current is not None:
+                path.append(current.pos)
+                current = current.parent
+
+            return path[::-1]
+
+        neighbours = grid.getSurrounding(currentNode.pos)
+        children = []
+        for pos in neighbours:
+            children.append(Node(pos, currentNode))
+
+        for child in children:
+            for closed in closedNodes:
+                if child.pos == closed.pos:
+                    continue
+            
+
+            child.g = currentNode.g + 5
+            child.h = (child.pos[0] - end.pos[0]) ** 2 + (child.pos[1] - end.pos[1]) ** 2
+            child.f = child.g + child.h
+
+            
+            for node in openNodes:
+                if child.pos == node.pos and child.g > node.g:
+                    continue
+            
+
+            openNodes.append(child)
+
+if __name__ == "__main__":
+    grid = Grid(5, 5)
+    start = Node((0, 0), None)
+    end = Node((grid.width - 1, 0), None)
+
+    for y in range(grid.height - 1):
+        grid.setPos((1, y), 0)
+
+    path = Astar(grid, start, end)
+    #print(path)
+
+    screen = pygame.display.set_mode((1000, 1000))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((255, 255, 255))
+        grid.draw(screen, path, start, end)
+
+        pygame.display.flip()
+
+    pygame.quit()
---
+++ grid.py
@@ -0,0 +1,79 @@
+import pygame
+
+class Node:
+    def __init__(self, pos, parent):
+        self.pos = pos
+        self.parent = parent
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __str__(self) -> str:
+        return f"{self.pos} - F:{self.f}, G:{self.g}, H:{self.h}, PARENT:({self.parent})"
+
+class Grid:
+
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+        self.grid = {}
+
+        self.generateBlank()
+
+    def generateBlank(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                self.grid[(x, y)] = Node((x, y), None)
+
+    def getSurrounding(self, pos):
+        neighbours = []
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                newPos = (x + pos[0], y + pos[1])
+                if self.grid.keys().__contains__(newPos):
+                    if self.grid[newPos] != 0:
+                        neighbours.append(newPos)
+
+        return neighbours
+
+    def draw(self, screen: pygame.Surface, path, start, end):
+        gridX = screen.get_size()[0] / self.width
+        gridY = screen.get_size()[1] / self.height
+        for key, value in self.grid.items():
+            realX = key[0] * gridX
+            realY = key[1] * gridY
+            rect = pygame.Rect(realX, realY, gridX, gridY)
+
+            if key == start.pos:
+                pygame.draw.rect(screen, (0, 150, 0), rect)
+
+            elif key == end.pos:
+                pygame.draw.rect(screen, (0, 0, 150), rect)
+
+            elif key in path:
+                pygame.draw.rect(screen, (150, 0, 0), rect)
+
+            elif value == 0:
+                pygame.draw.rect(screen, (0, 0, 0), rect)
+
+            else:
+                pygame.draw.rect(screen, (0, 0, 0), rect, 1)
+
+    def setPos(self, pos, value):
+        self.grid[pos] = value
+
+    def getPos(self, pos):
+        return self.grid[pos]
+
+    def positions(self):
+        return list(self.grid.keys())
+
+    def __getitem__(self, pos):
+        return self.grid[pos]
+
+    def __setitem__(self, pos, value) -> None:
+        self.grid[pos] = value
+
+if __name__ == "__main__":
+    grid = Grid(10, 10)
